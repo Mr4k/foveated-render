248998a627b300b7f66ae2f781d22eb5b4049334
Peter Stefek <peterlikesemail@gmail.com>
2020-10-02 21:55:41 -0700

    interp beginnings

--- create_pattern.py
+++ create_pattern.py
@@ -62,16 +62,17 @@ def create_dst_map_from_indexes(name, indexes, weights):
     # there are definitely bad cases here (such as when one 
     # point is directly past another point in the same direction)
     index_map = np.uint16(np.reshape(indexes, (IN_IMAGE_WIDTH, IN_IMAGE_HEIGHT, -1)))
-    weight_map = np.reshape(weights, (IN_IMAGE_WIDTH, IN_IMAGE_HEIGHT, -1))
+    weight_map = np.reshape(weights, (IN_IMAGE_WIDTH, IN_IMAGE_HEIGHT, - 1))
 
     # channels are in order of least significant bit
     # could make this much cleaner... (but the rest of the code is pretty bad rn)
     channel1 = np.bitwise_and(index_map // OUT_IMAGE_SIZE, 255)
-    channel2 = np.bitwise_and(weight_map % OUT_IMAGE_SIZE, 255)
-    channel3 = np.zeros(channel1.shape)
+    channel2 = np.bitwise_and(index_map % OUT_IMAGE_SIZE, 255)
+    channel3 = weight_map * 255
 
     dst_pix_map = np.dstack((channel1, channel2, channel3))
     dst_pix_map = np.swapaxes(dst_pix_map, 0, 1)
+    print(dst_pix_map)
 
     vals[0] /= IN_IMAGE_WIDTH
     vals[1] /= IN_IMAGE_HEIGHT
@@ -93,21 +94,18 @@ def create_dst_map_from_indexes(name, indexes, weights):
 
 def test_reconstruction(src_map_name, dst_map_names):
     src_pix_map = cv2.imread(src_map_name) / 255.0
-    dst_pix_maps = map(lambda name: cv2.imread(name, cv2.IMREAD_UNCHANGED), dst_map_names)
-
+    dst_pix_maps = list(map(lambda name: cv2.imread(name, cv2.IMREAD_UNCHANGED), dst_map_names))
     plt.imshow(src_pix_map)
     plt.show()
 
     # test that the decoding works
-    dst_pix_map = dst_pix_map.astype(np.uint16)
-    #dst_pix_map = np.float64(dst_pix_map * 32768)/32768
     im = np.zeros((228, 512, 3))
     for i in range(0,228):
         for j in range(0, 512):
             for dst_pix_map in dst_pix_maps:
                 x = dst_pix_map[i, j][0]
                 y = dst_pix_map[i, j][1]
-                w = dst_pix_map[i, j][2]
+                w = dst_pix_map[i, j][2] / 255.0
                 im[i,j][0:2] += src_pix_map[x,y][0:2] * w
     plt.imshow(im)
     plt.show()
@@ -116,8 +114,14 @@ distances, indexes = kdt.query(source_pixels, k = 2)
 
 create_src_map('src_map.png')
 
-weights = distances / distances.sum()
-print(weights)
+# there has to be a better numpy way of doing this...
+normalizing_consts = distances.sum(axis=1)
+weights = np.array([
+    distances[:, 0] / normalizing_consts,
+    distances[:, 1] / normalizing_consts
+])
 
-create_dst_map_from_indexes('dst_map_1.png', indexes[0, :])
-create_dst_map_from_indexes('dst_map_2.png', indexes[1, :])
+create_dst_map_from_indexes('dst_map_1.png', indexes[:, 0], weights[0, :])
+create_dst_map_from_indexes('dst_map_2.png', indexes[:, 1], weights[1, :])
+
+test_reconstruction('src_map.png', ['dst_map_1.png', 'dst_map_2.png'])
